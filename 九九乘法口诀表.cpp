30178b311f3f29f4d75a343a0855d07cd7ef2ca9
Cfan <33353151+xiezhiwei1020@users.noreply.github.com>
2018-04-04 11:40:50 +0800

    Update 九九乘法口诀表.cpp

--- 九九乘法口诀表.cpp
+++ 九九乘法口诀表.cpp
@@ -7,8 +7,8 @@ int main()
 	for(x=1;x<=9;x++)
 	{
 		for(y=1;y<=x;y++)
-			cout<<y<<"*"<<x<<"="<<x*y;
-		    cout<<endl;
+		   cout<<y<<"*"<<x<<"="<<x*y;
+		   cout<<endl;
 	}
 	system("pause");
 	return 0;
